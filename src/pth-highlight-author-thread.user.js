443f2828d6de81aad576b683d37e679aa1b60052
SavageCore <talk@savagecore.eu>
2016-12-04 06:45:02 +0000

    Correct formatting, xo wasn't linting!

--- src/pth-highlight-author-thread.user.js
+++ src/pth-highlight-author-thread.user.js
@@ -14,34 +14,34 @@
 /*	global document GM_addStyle	*/
 
 (function () {
-		'use strict';
+	'use strict';
 		// Append CSS to document
-		GM_addStyle('.sc_highlight_author_thread { background-color: #FF7043 !important}'); // eslint-disable-line new-cap
+	GM_addStyle('.sc_highlight_author_thread { background-color: #FF7043 !important}'); // eslint-disable-line new-cap
 
 		// Get userid
-		var userinfoElement = document.getElementsByClassName('username')[0];
-		var userid = userinfoElement.href.match(/user\.php\?id=(\d+)/)[1];
+	var userinfoElement = document.getElementsByClassName('username')[0];
+	var userid = userinfoElement.href.match(/user\.php\?id=(\d+)/)[1];
 
-		var table = document.getElementsByClassName('forum_index')[0];
-		var tbody = document.querySelectorAll('.forum_index > tbody')[0];
+	var table = document.getElementsByClassName('forum_index')[0];
+	var tbody = document.querySelectorAll('.forum_index > tbody')[0];
 
-		findThreads(table);
+	findThreads(table);
 
 		// Observe the page for more rows added to table
-		MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-		var obs = new MutationObserver(function (mutations, observer) {
-				var rows = mutations[0].addedNodes;
+	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	var obs = new MutationObserver(function (mutations, observer) {
+		var rows = mutations[0].addedNodes;
 				// Start at 1 to skip 'page-info' row inserted by Infinite Scroll
-				for (var i = 1; i < rows.length; i++) {
-					if (userid === rows[i].cells[3].firstChild.href.match(/user\.php\?id=(\d+)/)[1]) {
-						rows[i].className = 'sc_highlight_author_thread';
-					}
-				}
-		});
+		for (var i = 1; i < rows.length; i++) {
+			if (userid === rows[i].cells[3].firstChild.href.match(/user\.php\?id=(\d+)/)[1]) {
+				rows[i].className = 'sc_highlight_author_thread';
+			}
+		}
+	});
 
-		obs.observe(tbody, {
-			childList: true,
-		});
+	obs.observe(tbody, {
+		childList: true
+	});
 
 	function findThreads(table) {
 		// Loop all threads
